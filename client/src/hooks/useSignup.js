11fcb9e547c7103709b10a2c2432b45082c8a26c
Umair Shad <151988665+Shad-developer@users.noreply.github.com>
2024-08-18 16:25:29 +0500

    Update useSignup.js

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -23,7 +23,7 @@ const useSignup = () => {
     if (!success) return;
     setLoading(true);
     try {
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch("https://mern-chat-app-swart.vercel.app/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
